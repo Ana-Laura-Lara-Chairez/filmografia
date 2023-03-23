1f1c284df4a79f02ec2f3ccfc335b19f93a9dbee
Edgar Maldoando <medgarin1@gmail.com>
2023-03-22 19:24:29 -0600

    Feature (MW-003) Test Component
    -Create folder for components
    -Creatge test component with all requirements

---
+++ src/components/Test.tsx
@@ -0,0 +1,10 @@
+import { Filmografia } from '@/interfaces/Filmografia'
+import { FC } from 'react'
+
+type Props = {
+  filmografias: Filmografia
+}
+
+export const Test: FC<Props> = ({ filmografias }) => {
+  return <div>Test</div>
+}
